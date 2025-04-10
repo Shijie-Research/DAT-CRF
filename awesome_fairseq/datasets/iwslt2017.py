4507954334bb94330e0e1255dfaeaeb8712af142
Shi-Jie <Shijie.Li@student.uts.edu.au>
2025-04-10 11:33:43 +1000

    add new features

--- awesome_fairseq/datasets/iwslt2017.py
+++ awesome_fairseq/datasets/iwslt2017.py
@@ -25,6 +25,10 @@ lang2model.update(
 )
 
 
+def normalize_text(text):
+    return text.replace("‘", "'").replace("’", "'").replace("“", '"').replace("”", '"').replace("—", "-")
+
+
 @Dataset.register("iwslt17_en_zh")
 class TEDIWSLT2017(Dataset):
     NAME = "fairseq_iwslt2017"
@@ -45,39 +49,57 @@ class TEDIWSLT2017(Dataset):
         task_dir = os.path.join(HF_DATASETS_CACHE, cls.NAME, task)
         os.makedirs(task_dir, exist_ok=True)
 
-        if sep:
-            src_tokenizer = AutoTokenizer.from_pretrained(lang2model[lang1])
-            tgt_tokenizer = AutoTokenizer.from_pretrained(lang2model[lang2])
-            cls.TOKENIZER_CONFIGS.update({"bert_model_name": tgt_tokenizer.name_or_path})
-        else:
-            tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
-            cls.TOKENIZER_CONFIGS.update({"bert_model_name": tokenizer.name_or_path})
-
         binarized = os.path.join(task_dir, "binarized" + ("-sep" if sep else "-joined"))
         if not os.path.exists(binarized):
             data_dir = os.path.join(task_dir, "tokenized")
             os.makedirs(data_dir, exist_ok=True)
 
-            data = datasets.load_dataset(cls.HF_PATH, pair=f"{lang1}-{lang2}", is_multilingual=False)
+            data = datasets.load_dataset(
+                cls.HF_PATH,
+                pair=f"{lang1}-{lang2}",
+                is_multilingual=False,
+                trust_remote_code=True,
+            )
+
+            if sep:
+                src_tokenizer = AutoTokenizer.from_pretrained(lang2model[lang1])
+                tgt_tokenizer = AutoTokenizer.from_pretrained(lang2model[lang2])
+                cls.TOKENIZER_CONFIGS.update({"bert_model_name": tgt_tokenizer.name_or_path})
+            else:
+                tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
+                cls.TOKENIZER_CONFIGS.update({"bert_model_name": tokenizer.name_or_path})
+
             for split in ["train", "validation", "test"]:
                 name = "valid" if split == "validation" else split
 
                 if sep:
-                    with open(os.path.join(data_dir, f"{name}.{lang1}"), "w") as f:
-                        for text in tqdm(data[split]["translation"]):
-                            f.write(" ".join(src_tokenizer.tokenize(text[lang1].lower())) + "\n")
+                    # with open(os.path.join(data_dir, f"{name}.{lang1}"), "w") as f:
+                    #     for text in tqdm(data[split]["translation"]):
+                    #         text = text[lang1].lower()
+                    #         if lang1 == "zh":
+                    #             text = normalize_text(text)
+                    #         f.write(" ".join(src_tokenizer.tokenize(text)) + "\n")
 
                     with open(os.path.join(data_dir, f"{name}.{lang2}"), "w") as f:
                         for text in tqdm(data[split]["translation"]):
-                            f.write(" ".join(tgt_tokenizer.tokenize(text[lang2].lower())) + "\n")
+                            text = text[lang2].lower()
+                            if lang2 == "zh":
+                                text = normalize_text(text)
+                            f.write(" ".join(tgt_tokenizer.tokenize(text)) + "\n")
                 else:
                     with open(os.path.join(data_dir, f"{name}.{lang1}"), "w") as f:
                         for text in tqdm(data[split]["translation"]):
-                            f.write(" ".join(tokenizer.tokenize(text[lang1].lower())) + "\n")
+                            text = text[lang1].lower()
+                            if lang1 == "zh":
+                                text = normalize_text(text)
+                            f.write(" ".join(tokenizer.tokenize(text)) + "\n")
 
                     with open(os.path.join(data_dir, f"{name}.{lang2}"), "w") as f:
                         for text in tqdm(data[split]["translation"]):
-                            f.write(" ".join(tokenizer.tokenize(text[lang2].lower())) + "\n")
+                            text = text[lang2].lower()
+                            if lang2 == "zh":
+                                text = normalize_text(text)
+                            f.write(" ".join(tokenizer.tokenize(text)) + "\n")
 
             preprocess_args = ConfigsDict()
 
